b8d5b8c7b081b659072e12d9be5f305b8f5205dc
trknd92343 <75336478+trknd92343@users.noreply.github.com>
2020-12-01 12:14:36 -0800

    Add files via upload

---
+++ the-key_3_of_5.hx
@@ -0,0 +1,11 @@
+# THIS FILE IS A HORCRUX.
+# IT IS ONE OF 5 HORCRUXES THAT EACH CONTAIN PART OF AN ORIGINAL FILE.
+# THIS IS HORCRUX NUMBER 3.
+# IN ORDER TO RESURRECT THIS ORIGINAL FILE YOU MUST FIND THE OTHER 4 HORCRUX(ES) AND THEN BIND THEM USING THE PROGRAM FOUND AT THE FOLLOWING URL
+# https://github.com/jesseduffield/horcrux
+
+-- HEADER --
+{"originalFilename":"the-key.key","timestamp":1606852975,"index":3,"total":5,"threshold":3,"keyFragment":"Q7NO5XtTvQ508Y5ArApkMLEg6pYzkepa49L+S4s0v/px"}
+-- BODY --
+]"H„NÊ*C÷Öêuí	[²Ý‚›šòò”åQ‰LÝÿîØ½h…Ì0
+/8”å—D—5qÀÄ¯ó×ìÔ$²QƒT‚ûbJ2ªóàmJpŠþYX,q­®'þÁ€JÊp8º´ò±ÅFPÑo†lfôwguÙÚÂ·¾¾
